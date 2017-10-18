af9bf9b6f92cabeb1bd15359e57d0c0c6f34efb5
Ronny Reichmann <ronny.reichmann@icloud.com>
2017-10-18 22:27:45 +0200

    More tests, refinements

--- storage.js
+++ storage.js
@@ -15,7 +15,6 @@ function environment (config) {
   if (!config.maxDbs) throw new Error('config.maxDbs missing')
   config.maxSize = config.maxSize || 2 * 1024 * 1024 * 1024
   config.path = path.resolve(config.path)
-  console.log('resolved environment path:', config.path)
   let env = environmentForPath[config.path]
   if (!env) {
     mkdirp(config.path)
@@ -35,17 +34,17 @@ function environment (config) {
       composit.close = undefined
       composit.storage = undefined
     },
-    storage: name => composeStorage(environment, name)
+    storage: name => composeStorage(env, name)
   }
   return composit
 }
 
-function composeTransactionHandler () {
+function composeTransactionHandler (env) {
   let txn = null
   let counter = 0
   function begin () {
     if (counter === 0) {
-      txn = environment.beginTxn()
+      txn = env.beginTxn()
     }
     counter += 1
     return txn
@@ -62,36 +61,37 @@ function composeTransactionHandler () {
 
 function dataBufferFromValueAndAttachment (value, attachment) {
   const valueJson = JSON.stringify(value)
-  const valueJsonBuf = Buffer.from(valueJson)
-  const valueLengthBuf = Buffer.alloc(7)
-  valueLengthBuf.writeUInt32LE(valueJsonBuf.length, 0)
-  return Buffer.concat(
-    attachment
-    ? [valueLengthBuf, valueJsonBuf, attachment]
-    : [valueLengthBuf, valueJsonBuf])
+  const valueJsonLen = Buffer.byteLength(valueJson)
+  const attachmentLen = attachment ? attachment.length : 0
+  const attachmentStartIdx = 8 + valueJsonLen
+  const dataBuffer = Buffer.alloc(attachmentStartIdx + attachmentLen)
+  dataBuffer.writeDoubleBE(valueJsonLen, 0)
+  dataBuffer.write(valueJson, 8)
+  if (attachmentLen) dataBuffer.fill(attachment, attachmentStartIdx)
+  return dataBuffer
 }
 function valueAndAttachmentFromDataBuffer (dataBuffer) {
-  const valueLength = dataBuffer.readUInt32LE(0)
-  const separatorIndex = 7 + valueLength
-  const valueJson = dataBuffer.toString('utf8', 7, separatorIndex)
+  const valueJsonLen = dataBuffer.readDoubleBE(0)
+  const attachmentStartIdx = 8 + valueJsonLen
+  const valueJson = dataBuffer.toString('utf8', 8, attachmentStartIdx)
   const value = JSON.parse(valueJson)
-  const attachment = dataBuffer.length > separatorIndex
-    ? dataBuffer.slice(separatorIndex)
-    : undefined
-  return { value, attachment }
+  if (dataBuffer.length > attachmentStartIdx) {
+    const attachment = dataBuffer.slice(attachmentStartIdx)
+    return { value, attachment }
+  } else return value
 }
 
 define('type: teth-storage, retrieve: data-buffer-from-value-and-attachment-fn', () => dataBufferFromValueAndAttachment)
 define('type: teth-storage, retrieve: value-and-attachment-from-data-buffer-fn', () => valueAndAttachmentFromDataBuffer)
 
-function composeStorage (environment, name) {
-  if (!environment) throw new Error('Argument <environment> missing')
+function composeStorage (env, name) {
+  if (!env) throw new Error('Argument <env> missing')
   if (!name) throw new Error('Argument <name> missing')
-  let dbi = environment.openDbi({
+  let dbi = env.openDbi({
     name: name,
     create: true
   })
-  const txnHandler = composeTransactionHandler()
+  const txnHandler = composeTransactionHandler(env)
   function put (key, value, attachment) {
     const txn = txnHandler.begin()
     return pipe(resolve => {
@@ -120,37 +120,46 @@ function composeStorage (environment, name) {
       txnHandler.commit()
     })
   }
-  // function keys () {
-  //   const txn = txnHandler.begin()
-  //   return pipe(resolve => {
-  //     const cursor = new Cursor(txn, dbi)
-  //     let key = cursor.goToFirst()
-  //     let counter = 0
-  //     if (!key) {
-  //       resolve(counter)
-  //       txnHandler.commit()
-  //     } else {
-  //       return next => {
-  //         next(key)
-  //         counter += 1
-  //         key = cursor.goToNext()
-  //         if (!key) {
-  //           resolve(counter)
-  //           txnHandler.commit()
-  //         }
-  //       }
-  //     }
-  //   })
-  // }
-  const composit = { put, get, del, /*keys,*/ close }
+  function count () {
+    const txn = txnHandler.begin()
+    return pipe(resolve => {
+      const stat = dbi.stat(txn)
+      resolve(stat.entryCount)
+      txnHandler.commit()
+    })
+  }
+  function keys () {
+    const txn = txnHandler.begin()
+    return pipe(resolve => {
+      const cursor = new Cursor(txn, dbi)
+      let key = cursor.goToFirst()
+      let counter = 0
+      if (!key) {
+        resolve(counter)
+        txnHandler.commit()
+      } else {
+        return next => {
+          next(key)
+          counter += 1
+          key = cursor.goToNext()
+          if (!key) {
+            resolve(counter)
+            txnHandler.commit()
+          }
+        }
+      }
+    })
+  }
+  const composit = { put, get, del, count, keys, close }
   function close () {
     dbi.close()
     dbi = null
-    environment = null
+    env = null
     composit.put = undefined
     composit.get = undefined
     composit.del = undefined
-    // composit.keys = undefined
+    composit.count = undefined
+    composit.keys = undefined
     composit.close = undefined
   }
   return composit
--- storage.test.js
+++ storage.test.js
@@ -1,12 +1,16 @@
 /* Copyright 2017 Ronny Reichmann */
-/* globals test expect */
+/* globals test expect afterAll */
 
 const { send } = require('teth/T')
+const pipe = require('teth/pipe')
 const { environment } = require('./storage')
-const mockUserData = require('./storage-test-data.json')
+const testData = require('./storage-test-data.json')
 
 const dataBufferFromValueAndAttachment = send.sync('type: teth-storage, retrieve: data-buffer-from-value-and-attachment-fn')
 const valueAndAttachmentFromDataBuffer = send.sync('type: teth-storage, retrieve: value-and-attachment-from-data-buffer-fn')
+
+const env = environment({maxDbs: 1, path: './storage-test-environment'})
+const store = env.storage('test-storage')
 
 test('test value and attachment', () => {
   const attachment = { hello: 'World!' }
@@ -30,6 +34,72 @@ test('test value without attachment', () => {
   const dataBuffer = dataBufferFromValueAndAttachment(value)
   const result = valueAndAttachmentFromDataBuffer(dataBuffer)
 
-  expect(result.value).toEqual(value)
-  expect(result.attachment).toBeUndefined()
+  expect(result).toEqual(value)
+})
+
+function testBatchPut (store, allItems, done) {
+  const allItemIds = allItems.map(i => i.id)
+  pipe.all(allItems.map(i => store.put(i.id, i)))
+    .then(allResultKeys => {
+      allResultKeys.forEach(key => {
+        const idx = allItemIds.indexOf(key)
+        expect(idx).toBeGreaterThan(-1)
+      })
+      done()
+    })
+    .catch(err => {
+      expect(err).toBe(null)
+      done()
+    })
+}
+function testBatchGet (store, allItems, done) {
+  pipe.all(allItems.map(i => store.get(i.id)))
+    .then(allRetrievedItems => {
+      expect(allRetrievedItems).toEqual(allItems)
+      done()
+    })
+    .catch(err => {
+      expect(err).toBe(null)
+      done()
+    })
+}
+
+function batchCall (label, batchFn) {
+  for (var i = 0; i < 5; i++) {
+    const start = i * 1000
+    test(`${label} ${i + 1}. 1000 test items`, done => {
+      batchFn(store, testData.slice(start, start + 1000), done)
+    })
+  }
+}
+
+batchCall('putting', testBatchPut)
+batchCall('getting', testBatchGet)
+
+test('count', done => {
+  store.count().then(count => {
+    expect(count).toBeGreaterThan(0)
+    done()
+  })
+})
+
+test('iterating keys', done => {
+  store.keys()
+    .forEach(key => {
+      console.log(key)
+    })
+    .then(count => {
+      console.log('count', count)
+      done()
+    })
+    .catch(err => {
+      expect(err).toBe(null)
+    })
+})
+
+// TODO: KEYS TESTS
+
+afterAll(() => {
+  store.close()
+  env.close()
 })
